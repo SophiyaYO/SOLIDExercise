d4051e1df6fb4d7336527a2feafe6e96863de79e
SophYo <sophiya.dimitrova@gmail.com>
2019-07-23 18:27:37 +0300

    created and implements methods from the interface

---
+++ src/loggerLibrary/appenders/FileAppender.java
@@ -0,0 +1,16 @@
+package loggerLibrary.appenders;
+
+import loggerLibrary.appenders.interfaces.Appender;
+import loggerLibrary.enumerations.ReportLevel;
+
+public class FileAppender implements Appender {
+    @Override
+    public void append(String date, ReportLevel reportLevel, String message) {
+
+    }
+
+    @Override
+    public void setReportLevel(ReportLevel reportLevel) {
+
+    }
+}
