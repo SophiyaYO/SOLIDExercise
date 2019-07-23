985248d17d1d96362f624b46fddff22a4706ed25
SophYo <sophiya.dimitrova@gmail.com>
2019-07-23 18:22:24 +0300

    logMessage() added

--- src/loggerLibrary/loggers/interfaces/MessageLogger.java
+++ src/loggerLibrary/loggers/interfaces/MessageLogger.java
@@ -1,6 +1,7 @@
 package loggerLibrary.loggers.interfaces;
 
 import loggerLibrary.appenders.interfaces.Appender;
+import loggerLibrary.enumerations.ReportLevel;
 
 public class MessageLogger implements Logger {
     private Appender[] appenders;
@@ -12,26 +13,36 @@ public class MessageLogger implements Logger {
 
     @Override
     public void logInfo(String date, String message) {
-
+       this.logMessage(date, ReportLevel.INFO, message);
     }
 
     @Override
     public void logWarning(String date, String message) {
+        this.logMessage(date, ReportLevel.WARNING, message);
 
     }
 
     @Override
     public void logError(String date, String message) {
+        this.logMessage(date, ReportLevel.ERROR, message);
 
     }
 
     @Override
     public void logCritical(String date, String message) {
+        this.logMessage(date, ReportLevel.CRITICAL, message);
 
     }
 
     @Override
     public void logFatal(String date, String message) {
+        this.logMessage(date, ReportLevel.FATAL, message);
 
+    }
+
+    private void logMessage(String date, ReportLevel reportLevel, String message) {
+        for (Appender appender : this.appenders) {
+            appender.append(date, reportLevel ,message);
+        }
     }
 }
