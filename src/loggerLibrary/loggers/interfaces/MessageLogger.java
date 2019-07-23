b981480c4e58580aefceaa9406267d3129e6acde
SophYo <sophiya.dimitrova@gmail.com>
2019-07-23 18:16:06 +0300

    initial commit

---
+++ src/loggerLibrary/loggers/interfaces/MessageLogger.java
@@ -0,0 +1,28 @@
+package loggerLibrary.loggers.interfaces;
+
+public class MessageLogger implements Logger {
+    @Override
+    public void logInfo(String date, String message) {
+
+    }
+
+    @Override
+    public void logWarning(String date, String message) {
+
+    }
+
+    @Override
+    public void logError(String date, String message) {
+
+    }
+
+    @Override
+    public void logCritical(String date, String message) {
+
+    }
+
+    @Override
+    public void logFatal(String date, String message) {
+
+    }
+}
