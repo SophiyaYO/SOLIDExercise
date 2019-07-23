36aa5e8b29b84500e3ceefeb9b83349d869dffeb
SophYo <sophiya.dimitrova@gmail.com>
2019-07-23 15:46:12 +0300

    added implementation Layout and overrides the method from

--- src/loggerLibrary/layouts/interfaces/simpleLayout.java
+++ src/loggerLibrary/layouts/interfaces/simpleLayout.java
@@ -1,8 +1,14 @@
 package loggerLibrary.layouts.interfaces;
 
-public class simpleLayout {
+public class simpleLayout implements Layout {
 
     public simpleLayout() {
 
     }
+
+    @Override
+    public String format(String date, String reportLevel, String message) {
+        return String.format("%s - %s - %s");
+    }
 }
+
