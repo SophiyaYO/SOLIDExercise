c2c19cb1a770dcf3de1d708b8bc1fa0e7519d07d
SophYo <sophiya.dimitrova@gmail.com>
2019-07-23 15:52:34 +0300

    initial personal test code(not from requirements)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import loggerLibrary.layouts.interfaces.Layout;
+import loggerLibrary.layouts.interfaces.simpleLayout;
+
 public class Main {
     public static void main(String[] args) {
 
+        Layout layout = new simpleLayout();
+
+        System.out.println(
+                layout.format(
+                "3/31/2015 5:33:07 PM",
+                "ERROR",
+                "Error parsing request"));
     }
 }
