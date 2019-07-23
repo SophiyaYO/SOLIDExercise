27f1d8e37d626d250d851238f63ee9d18a945477
SophYo <sophiya.dimitrova@gmail.com>
2019-07-23 17:21:16 +0300

    setReportLevel() added

--- src/loggerLibrary/appenders/interfaces/Appender.java
+++ src/loggerLibrary/appenders/interfaces/Appender.java
@@ -1,5 +1,9 @@
 package loggerLibrary.appenders.interfaces;
 
+import loggerLibrary.enumerations.ReportLevel;
+
 public interface Appender {
     void append(String date, String reportLevel, String message);
+
+    void setReportLevel(ReportLevel reportLevel);
 }
