81a5d5221ce15e01ba917ef3ee08038c4052f068
SophYo <sophiya.dimitrova@gmail.com>
2019-07-23 17:11:12 +0300

    field and constructor added

--- src/loggerLibrary/appenders/ConsoleAppender.java
+++ src/loggerLibrary/appenders/ConsoleAppender.java
@@ -1,10 +1,17 @@
 package loggerLibrary.appenders;
 
 import loggerLibrary.appenders.interfaces.Appender;
+import loggerLibrary.layouts.interfaces.Layout;
 
 public class ConsoleAppender implements Appender {
+    private Layout layout;
+
+    public ConsoleAppender(Layout layout) {
+        this.layout = layout;
+    }
+
     @Override
     public void append() {
-
+        System.out.println();
     }
 }
