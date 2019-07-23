13c8d01a782c1c3de8128e64406e775624175459
SophYo <sophiya.dimitrova@gmail.com>
2019-07-23 15:58:26 +0300

    removed empty constructor

--- src/loggerLibrary/layouts/interfaces/simpleLayout.java
+++ src/loggerLibrary/layouts/interfaces/simpleLayout.java
@@ -2,10 +2,6 @@ package loggerLibrary.layouts.interfaces;
 
 public class simpleLayout implements Layout {
 
-    public simpleLayout() {
-
-    }
-
     @Override
     public String format(String date, String reportLevel, String message) {
         return String.format("%s - %s - %s",
